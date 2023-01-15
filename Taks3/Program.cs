606e792b06843f5e11b536ac3263f9ad136716dc
Iris <royx0227@gmail.com>
2023-01-15 22:47:28 +0300

    Taks 3

---
+++ Taks3/Program.cs
@@ -0,0 +1,15 @@
+﻿/* Напишите программу, которая на вход принимает число и выдает его квадрат (число умноженное
+на само себя).
+
+Например:
+4 -> 16
+-3 -> 9
+-7 -> 49
+*/
+
+Console.WriteLine("Введите число");
+string numberUser = Console.ReadLine();
+int number = Convert.ToInt32(numberUser);
+
+int square = number * number;
+Console.WriteLine(square);

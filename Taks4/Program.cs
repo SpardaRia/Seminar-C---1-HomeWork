584b4764647e4b77e496e22a368d071048a85be5
Iris <royx0227@gmail.com>
2023-01-16 00:21:44 +0300

    Taks 4 Незаконченое

---
+++ Taks4/Program.cs
@@ -0,0 +1,38 @@
+﻿/* Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+2, 3, 7 -> 7
+44 5 78 -> 78
+22 3 9 -> 22
+*/
+
+Console.WriteLine("Введи первое число: ");
+string numberOneUser = Console.ReadLine();
+int numberOne = Convert.ToInt32(numberOneUser);
+
+Console.WriteLine("Введите второе число: ");
+string numberTwoUser = Console.ReadLine();
+int numberTwo = Convert.ToInt32(numberTwoUser);
+
+Console.WriteLine("Введите трертье число: ");
+string numberThreeUser = Console.ReadLine();
+int numberThree = Convert.ToInt32(numberThreeUser);
+
+if (numberOne > numberTwo)
+{
+    int max = numberOne;
+}
+else if ( numberTwo > numberThree)
+{
+    int max = numberTwo;
+    Console.WriteLine($"Наибольшее число: {numberTwo}");
+}
+else if (numberOne > numberThree)
+{
+    int max = numberOne;
+    Console.WriteLine($"Наибольшее число: {numberOne}");
+}
+else 
+{
+    int max = numberThree;
+    Console.WriteLine($"Наибольшее число: {numberThree}");
+}
